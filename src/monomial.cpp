2e4750930be4d6666e8ff95916f50aee5eb386f2
Ilya <xmikasax.main@gmail.com>
2020-03-20 16:50:27 +0300

    Add basic monomial arithmetic functions Change operator[] to Set function

--- src/monomial.cpp
+++ src/monomial.cpp
@@ -3,21 +3,79 @@
 
 namespace NLibPoly {
 
+TMonomial::TMonomial()
+    : Degree(0) {}
+
 TMonomial::TMonomial(TIndex index, TDegree degree) {
-    (*this)[index] = degree;
+    Variables[index] = degree;
     Degree = degree;
 }
 
-TMonomial::TDegree &TMonomial::operator[](TIndex index) {
+TMonomial::TMonomial(const std::initializer_list<TDegree> &init_list) {
+    TIndex index(0);
+    for (const auto &it : init_list) {
+        Variables[index] += it;
+        Degree += it;
+        ++index;
+    }
+}
+
+void TMonomial::Set(TIndex index, TDegree degree) {
+    TDegree diff = degree - Variables[index];
+    Variables[index] += diff;
+    Degree += diff;
+}
+
+TMonomial::TDegree TMonomial::operator[](TIndex index) {
     return Variables[index];
 }
 
-TMonomial::TDegree &TMonomial::at(TIndex index) {
+TMonomial::TDegree TMonomial::At(TIndex index) {
     return Variables.at(index);
 }
 
-const TMonomial::TDegree &TMonomial::at(TIndex index) const {
+const TMonomial::TDegree &TMonomial::At(TIndex index) const {
     return Variables.at(index);
+}
+
+const TMonomial::TDegree &TMonomial::GetDegree() const {
+    return Degree;
+}
+
+TMonomial &TMonomial::operator*=(const TMonomial &other) {
+    for (auto it : other.Variables) {
+        Variables[it.first] += it.second;
+        Degree += it.second;
+    }
+    return *this;
+}
+
+TMonomial TMonomial::operator*(const TMonomial &other) const {
+    TMonomial res(*this);
+    return res *= other;
+}
+
+TMonomial &TMonomial::operator/=(const TMonomial &other) {
+    for (auto it : other.Variables) {
+        Variables[it.first] -= it.second;
+        Degree -= it.second;
+    }
+    return *this;
+}
+
+TMonomial TMonomial::operator/(const TMonomial &other) const {
+    TMonomial res(*this);
+    return res /= other;
+}
+
+TMonomial Lcm(const TMonomial &a, const TMonomial &b) {
+    TMonomial res(a);
+    for (auto it : b.Variables) {
+        TMonomial::TDegree diff = std::max(res[it.first], it.second) - res[it.first];
+        res.Variables[it.first] += diff;
+        res.Degree += diff;
+    }
+    return res;
 }
 
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -10,10 +10,26 @@ void TestAll() {
 }
 
 void TestMonomial() {
-    NLibPoly::TMonomial monomial;
-    assert(monomial[0] == 0);
-    monomial[2] = 1;
-    assert(monomial[2] == 1);
+    NLibPoly::TMonomial a;
+    assert(a[0] == 0);
+    a.Set(2, 1);
+    assert(a[2] == 1);
+
+    NLibPoly::TMonomial b;
+    NLibPoly::TMonomial c = b / a;
+    assert(c.GetDegree() == -1);
+    assert(c[2] == -1);
+
+    NLibPoly::TMonomial d = b * a;
+    assert(d.GetDegree() == 1);
+    assert(d[2] == 1);
+
+    b.Set(3, 1);
+    NLibPoly::TMonomial e = NLibPoly::Lcm(b, d);
+
+    assert(e.GetDegree() == 2);
+    assert(e[2] == 1);
+    assert(e[3] == 1);
 
     std::cerr << "Monomial tests OK!" << std::endl;
 }
