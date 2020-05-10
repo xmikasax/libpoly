8383bfbccc85f04c9f8f973ec1b4e618f3b9aab1
Ilya <xmikasax.main@gmail.com>
2020-05-11 02:49:01 +0300

    Change tests to use block scope

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -17,32 +17,52 @@ void TestAll() {
 void TestMonomial() {
     using namespace NLibPoly;
 
-    TMonomial a;
-    assert(a.GetDegree(0) == 0);
-    a.SetDegree(2, 2);
-    assert(a.GetDegree(2) == 2);
+    {
+        TMonomial a;
+        assert(a.GetDegree(0) == 0);
+        a.SetDegree(2, 2);
+        assert(a.GetDegree(2) == 2);
+    }
 
-    TMonomial b{{2, 1}};
-    TMonomial c = a / b;
-    assert(c.GetDegree(0) == 0);
-    assert(c.GetDegree(2) == 1);
+    {
+        TMonomial a{{2, 2}};
+        TMonomial b{{2, 1}};
+        TMonomial c = a / b;
+        assert(c.GetDegree(0) == 0);
+        assert(c.GetDegree(2) == 1);
+    }
 
-    TMonomial d = b * a;
-    assert(d.GetDegree(2) == 3);
+    {
+        TMonomial a{{2, 2}};
+        TMonomial b{{2, 1}};
+        TMonomial c = b * a;
+        assert(c.GetDegree(2) == 3);
+    }
 
-    b.SetDegree(3, 1);
-    TMonomial e = Lcm(b, d);
+    {
+        TMonomial a{{2, 2}};
+        TMonomial b{{2, 1}, {3, 1}};
+        TMonomial c = Lcm(a, b);
 
-    assert(e.GetDegree(2) == 3);
-    assert(e.GetDegree(3) == 1);
+        assert(c.GetDegree(2) == 2);
+        assert(c.GetDegree(3) == 1);
+    }
 
-    try {
-        TMonomial bad = b / a;
-        assert(false);
-    } catch (NUtils::TLibPolyException& e) {}
+    {
+        TMonomial a{{2, 2}};
+        TMonomial b{{2, 1}, {3, 1}};
+        try {
+            TMonomial bad = b / a;
+            assert(false);
+        } catch (NUtils::TLibPolyException& e) {}
+    }
 
-    assert(c * c == a);
-    assert(c != a);
+    {
+        TMonomial a{{2, 2}};
+        TMonomial b{{2, 1}};
+        assert(b * b == a);
+        assert(b != a);
+    }
 
     std::cerr << "Monomial tests OK!" << std::endl;
 }
@@ -50,28 +70,41 @@ void TestMonomial() {
 void TestTerm() {
     using namespace NLibPoly;
 
-    TTerm<size_t> a;
-    assert(a.GetDegree(0) == 0);
-    a.SetDegree(2, 1);
-    a.SetCoefficient(2);
-    assert(a.GetDegree(2) == 1);
-    assert(a.GetCoefficient() == 2);
+    {
+        TTerm<size_t> a;
+        assert(a.GetDegree(0) == 0);
+        a.SetDegree(2, 1);
+        a.SetCoefficient(2);
+        assert(a.GetDegree(2) == 1);
+        assert(a.GetCoefficient() == 2);
+    }
 
-    TTerm<size_t> b{{2, 1}};
-    TTerm<size_t> c = a / b;
-    assert(c.GetCoefficient() == 2);
-    assert(c.GetDegree(2) == 0);
+    {
+        TTerm<size_t> a(2, {{2, 1}});
+        TTerm<size_t> b{{2, 1}};
+        TTerm<size_t> c = a / b;
+        assert(c.GetCoefficient() == 2);
+        assert(c.GetDegree(2) == 0);
+    }
 
-    TTerm<size_t> d = b * a;
-    assert(d.GetDegree(2) == 2);
-    assert(d.GetCoefficient() == 2);
+    {
+        TTerm<size_t> a(2, {{2, 1}});
+        TTerm<size_t> b{{2, 1}};
+        TTerm<size_t> d = b * a;
+        assert(d.GetDegree(2) == 2);
+        assert(d.GetCoefficient() == 2);
+    }
 
-    b.SetDegree(3, 1);
-    TTerm<size_t> e = Lcm(b, d);
+    {
+        TTerm<size_t> a(2, {{2, 2}});
+        TTerm<size_t> b{{2, 1}};
+        b.SetDegree(3, 1);
+        TTerm<size_t> c = Lcm(a, b);
 
-    assert(e.GetDegree(2) == 2);
-    assert(e.GetDegree(3) == 1);
-    assert(e.GetCoefficient() == 2);
+        assert(c.GetDegree(2) == 2);
+        assert(c.GetDegree(3) == 1);
+        assert(c.GetCoefficient() == 2);
+    }
 
     std::cerr << "Term tests OK!" << std::endl;
 }
@@ -79,20 +112,26 @@ void TestTerm() {
 void TestOrder() {
     using namespace NLibPoly;
 
-    TMonomial a{{1, 1}, {2, 1}};
-    TMonomial b{{1, 1}, {2, 1}};
+    {
+        TMonomial a{{1, 1}, {2, 1}};
+        TMonomial b{{1, 1}, {2, 1}};
 
-    assert(TLexOrder()(a, b) == 0);
+        assert(TLexOrder()(a, b) == 0);
+    }
 
-    TMonomial c{{1, 1}, {2, 1}};
-    TMonomial d{{1, 1}, {2, 2}};
+    {
+        TMonomial a{{1, 1}, {2, 1}};
+        TMonomial b{{1, 1}, {2, 2}};
 
-    assert(TLexOrder()(c, d) < 0);
+        assert(TLexOrder()(a, b) < 0);
+    }
 
-    TMonomial e{{1, 2}, {2, 1}};
-    TMonomial f{{1, 1}, {2, 1}};
+    {
+        TMonomial a{{1, 2}, {2, 1}};
+        TMonomial b{{1, 1}, {2, 1}};
 
-    assert(TLexOrder()(e, f) > 0);
+        assert(TLexOrder()(a, b) > 0);
+    }
 
     std::cerr << "Order tests OK!" << std::endl;
 }
